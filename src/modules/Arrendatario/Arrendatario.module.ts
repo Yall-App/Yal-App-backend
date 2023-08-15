62db0639a4e9116a15fa7a1cf7a35624d05a881e
SantiRosso <santiagorosso@live.com.ar>
2023-08-15 12:10:48 -0300

    typeORM

--- src/modules/Arrendatario/Arrendatario.module.ts
+++ src/modules/Arrendatario/Arrendatario.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ArrendatarioController } from './arrendatario.controller';
 import { ArrendatarioService } from './arrendatario.service';
+import { Arrendatario } from './entities/Arrendatario.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [], // Le queremos dar a este módulo alguna funcionalidad externa.
+  imports: [TypeOrmModule.forFeature([Arrendatario])], // Le queremos dar a este módulo alguna funcionalidad externa.
   providers: [ArrendatarioService], // Proveedores, en este caso los servicios.
   controllers: [ArrendatarioController], // Los controladores.
   exports: [], // Si quiero que este módulo exporte alguna fincionalidad que le sea propia.
--- src/modules/Arrendatario/entities/Arrendatario.entity.ts
+++ src/modules/Arrendatario/entities/Arrendatario.entity.ts
@@ -1,8 +1,18 @@
-export class ArrendatarioEntity {
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Arrendatario {
+  @PrimaryGeneratedColumn()
   id: string;
+
+  @Column()
   name: string;
+  @Column()
   apartment: string;
+  @Column()
   document: number;
+  @Column()
   email: string;
+  @Column()
   phoneNumber: number;
 }
